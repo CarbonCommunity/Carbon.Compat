ce0f307d87efd4ff1966e6bc6081524e3d4b77b6
raul <raulssorban@gmail.com>
2023-10-30 19:43:42 +0100

    Further compatibility

--- src/Patches/Oxide/OxideTypeRef.cs
+++ src/Patches/Oxide/OxideTypeRef.cs
@@ -135,6 +135,11 @@ public class OxideTypeRef : BaseOxidePatch
 	        return;
         }
 
+        if (type.FullName == "Oxide.Core.Plugins.PluginEvent")
+        {
+	        type.Namespace = string.Empty;
+        }
+
         if (type.Namespace.StartsWith("Newtonsoft.Json"))
         {
 	        type.Scope = CompatManager.Newtonsoft.ImportWith(importer);
@@ -152,6 +157,9 @@ public class OxideTypeRef : BaseOxidePatch
 
         if (type.Namespace.StartsWith("WebSocketSharp"))
         {
+	        if (type.Namespace == "WebSocketSharp.Net" && type.Name == "SslConfiguration")
+		        type.Name = "ClientSslConfiguration";
+
 	        type.Scope = CompatManager.wsSharp.ImportWith(importer);
 	        return;
         }
