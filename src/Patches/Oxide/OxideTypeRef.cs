92be927f56ef2c96ce365226672e84490305fafc
raul <raulssorban@gmail.com>
2025-11-11 17:04:55 +0100

    Revert "Libraries scope fix"
    This reverts commit fc074728fc950e65050516c46d7eefd30b8f5dde.

--- src/Patches/Oxide/OxideTypeRef.cs
+++ src/Patches/Oxide/OxideTypeRef.cs
@@ -124,10 +124,13 @@ public class OxideTypeRef : BaseOxidePatch
 		    return;
 	    }
 
-	    if (type.FullName is "Oxide.Core.Libraries")
-	    {
-		    goto common;
-	    }
+        if (type.Scope is TypeReference parent)
+        {
+            if (parent.FullName is "Oxide.Core.Libraries.Timer")
+            {
+                goto common;
+            }
+        }
 
         if (type.Scope is not AssemblyReference aref || !Helpers.IsOxideASM(aref))
         {
